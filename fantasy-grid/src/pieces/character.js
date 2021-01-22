21862ad763e34bbd24f909eee6b568ee37141b17
MitchRose976 <mitch.j.rose@outlook.com>
2021-01-22 12:28:29 -0500

    Edited Character Movement
    Characters have been edited. HP and attack values were added. Movement speed was added as well as a move method that allows the player to move to any square within the character movement speed

--- fantasy-grid/src/pieces/character.js
+++ fantasy-grid/src/pieces/character.js
@@ -1,7 +1,14 @@
 export default class Character {
-    constructor(player, characterIdentifier, playerColor) {
+    constructor(player, characterIdentifier, playerColor, characterHP) {
         this.player = player;
         this.characterIdentifier = characterIdentifier;
         this.style = {backgroundColor: playerColor};
+        this.playerHP = playerHP;
+    }
+    get characterHP() {
+        return this.characterHP;
+    }
+    set characterHP(value) {
+        this.characterHP = value;
     }
 };
--- fantasy-grid/src/pieces/druid.js
+++ fantasy-grid/src/pieces/druid.js
@@ -1,9 +1,31 @@
 import Character from './character.js';
 
 export default class Druid extends Character {
+    movementSpeed = 3;
+    characterHP = 100;
+    attackDamage = Math.floor(Math.random() * 30) + 1;
     constructor(player) {
-        super(player, "D", (player === 1? "red" : "blue"))
-    }      
+        super(player, "D", (player === 1? "red" : "blue"), this.characterHP)
+    }
+
+    isMovePossible(src, dest){
+        let possibleMoveArray = [];
+        for (let i = this.movementSpeed; i > 0; i--) {
+          possibleMoveArray.push(src - (9 * (i - 1))  
+                                ,src - (8 * i)   
+                                ,src - (7 *  (i - 1))   
+                                ,src + (1 * i)   
+                                ,src + (9 * (i - 1))   
+                                ,src + (8 * i)   
+                                ,src + (7 * (i - 1))   
+                                ,src - (1 * i))
+      }
+      return possibleMoveArray.includes(dest);
+        }
+          
+        getSrcToDestPath(src, dest){
+          return [];
+        }      
 };
  
 /*move anywhere as long as space is empty, and within the range of the player movement.*/
